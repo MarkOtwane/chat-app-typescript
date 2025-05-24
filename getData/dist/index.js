ae354579fcad3963323e343b6df0df15bc0edd0a
KING DAVID <otwanemark254@gmail.com>
2025-05-24 07:59:35 +0300

    Learn to use enum and filter method to interate threough the array

--- getData/dist/index.js
+++ getData/dist/index.js
@@ -3,11 +3,65 @@
 //     return `Hello, ${name}`
 // }
 // console.log(greet("mark"));
-let task = {
-    title: "mark Otwane",
-    completed: true
-};
-console.log(task);
-const enter = { name: "write a report", id: 12 };
-const admin = { name: "submit proposal", id: 23, role: "admin" };
-console.log(admin);
+//basic types :: primitive data types 
+// type Task = {
+//     title:string;
+//     completed:boolean;
+// };
+// let task : Task = {
+//     title: "mark Otwane",
+//     completed:true
+// }
+// console.log(task);
+// console.log('====================================');
+// console.log(Interface);
+// console.log('====================================');
+// interface User{
+//     name:string;
+//     id:number
+// }
+// interface Admin extends User{
+//     role: string
+// }
+// const enter : User = {name:"write a report", id:12}
+// const admin : Admin = {name:"submit proposal", id:23, role:"admin"}
+// console.log(admin);
+// --Enum--
+// enum Priority{
+//     Low = "low",
+//     High = "high",
+//     Medium = "medium"
+// }
+// interface Task {
+//     title:string;
+//     priority : Priority
+// }
+// const task : Task = {title: "write a report", priority: Priority.High}
+//  Exercise
+// Create an enum Status with Pending, InProgress, Completed.
+// Add status to the Task interface.
+// Write a function to filter tasks by status.
+// Reflect: How do enums make the API more maintainable?
+var Status;
+(function (Status) {
+    Status["Pending"] = "pending";
+    Status["InProgress"] = "inProgress";
+    Status["completed"] = "completed";
+})(Status || (Status = {}));
+//function to filter the task with priority
+const tasks = [
+    { name: "jone", status: Status.Pending }, //I had a bug in th declaration but i figured it out,
+    { name: "jack", status: Status.Pending },
+    { name: "james", status: Status.InProgress },
+    { name: "john", status: Status.Pending },
+    { name: "Leah", status: Status.InProgress },
+    { name: "Adin", status: Status.Pending },
+    { name: "Doe", status: Status.InProgress },
+    { name: "God", status: Status.completed }
+];
+//function to filter  Method
+const pendingTask = tasks.filter(task => task.status === Status.Pending);
+//filter method in ts
+// create a new element then you assgn it to the array.filter
+//const pending = tasks(array).filter(task=> task.status === Status.pending)
+console.log(pendingTask);
