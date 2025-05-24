c781bc03f21f47da7f94298748ad87c892de531e
KING DAVID <otwanemark254@gmail.com>
2025-05-24 08:23:34 +0300

    union and intersection

--- getData/src/index.ts
+++ getData/src/index.ts
@@ -50,44 +50,64 @@
         // Add status to the Task interface.
         // Write a function to filter tasks by status.
         // Reflect: How do enums make the API more maintainable?
+    // {
+    //     enum Status{
+    //         Pending = "pending",
+    //         InProgress = "inProgress",
+    //         completed= "completed"
+    //     }
+    //     interface  Task{
+    //         name:string;
+    //         status: Status,
+    //     }
+    //     //function to filter the task with priority
+    //     const tasks : Task[]=[
+    //         {name:"jone", status:Status.Pending}, //I had a bug in th declaration but i figured it out,
+    //         {name:"jack", status:Status.Pending},
+    //         {name:"james", status:Status.InProgress},
+    //         {name:"john", status:Status.Pending},
+    //         {name:"Leah", status:Status.InProgress},
+    //         {name:"Adin", status:Status.Pending},
+    //         {name:"Doe", status:Status.InProgress},
+    //         {name:"God", status:Status.completed}
+    //     ]
 
-        enum Status{
-            Pending = "pending",
-            InProgress = "inProgress",
-            completed= "completed"
-        }
-        interface  Task{
-            name:string;
-            status: Status,
-        }
-        //function to filter the task with priority
-        const tasks : Task[]=[
-            {name:"jone", status:Status.Pending}, //I had a bug in th declaration but i figured it out,
-            {name:"jack", status:Status.Pending},
-            {name:"james", status:Status.InProgress},
-            {name:"john", status:Status.Pending},
-            {name:"Leah", status:Status.InProgress},
-            {name:"Adin", status:Status.Pending},
-            {name:"Doe", status:Status.InProgress},
-            {name:"God", status:Status.completed}
-        ]
+    //     //function to filter  Method
+    //     const pendingTask = tasks.filter(task=>task.status === Status.Pending)
 
-        //function to filter  Method
-        const pendingTask = tasks.filter(task=>task.status === Status.Pending)
-
-        const completedTask = tasks.filter(task=>task.status===Status.completed)
-        const progressTask = tasks.filter(task=>task.status===Status.InProgress)
-               //filter method in ts
-               // create a new element then you assgn it to the array.filter
-               //const pending = tasks(array).filter(task=> task.status === Status.pending)
+    //     const completedTask = tasks.filter(task=>task.status===Status.completed)
+    //     const progressTask = tasks.filter(task=>task.status===Status.InProgress)
+    //            //filter method in ts
+    //            // create a new element then you assgn it to the array.filter
+    //            //const pending = tasks(array).filter(task=> task.status === Status.pending)
             
-        console.log(completedTask);
-        console.log('====================================');
-        console.log('====================================');
-        console.log(pendingTask);
-        console.log('====================================');
-        console.log('====================================');
-        console.log(progressTask);
-        console.log('====================================');
+    //     console.log(completedTask);
+    //     console.log('====================================');
+    //     console.log('====================================');
+    //     console.log(pendingTask);
+    //     console.log('====================================');
+    //     console.log('====================================');
+    //     console.log(progressTask);
+    //     console.log('====================================');
         
 
+    // }
+
+    // UNION TYPES & INTERSECTION TYPES
+    // Allows one to be in several types (|) (string | number)
+    //combine them use intersection (&) (string & number)
+
+    type Category = "work" | "personl" | "urgent";
+    interface Task{
+        title:string;
+        category: Category
+    }
+    interface Auditable{
+        createdAt:Date;
+    }
+
+    type AuditableTask  = Task & Auditable
+
+    const task: AuditableTask = 
+        {title: "Write a report", category: "work", createdAt:new Date()}
+        
