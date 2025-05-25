e33671af4b5284d0a8278cbfd04259786f407e24
KING DAVID <otwanemark254@gmail.com>
2025-05-25 19:11:25 +0300

    more on generics

--- getData/dist/generics.js
+++ getData/dist/generics.js
@@ -17,3 +17,11 @@ function getFirstNumber(array) {
 const numbers = [1, 2, 3, 7, 90, 345, 6, 8, 9];
 const firstnumber = getFirstNumber(numbers);
 console.log(firstnumber);
+// generics return the type of an element whene the element is noy defined like use of any nd unknown types so in this situation generics returns a value if its a number it returns a number and the string and boolean the same espeacially in the use of arrays https://youtu.be/EcCTIExsqmI?si=WaOlJ775SXd1J0X8
+// generics used here 
+function getNumber(array) {
+    return array[0];
+}
+const number12 = [1, 2, 3, 4, 5, 6, 7];
+const firstNumber23 = getNumber(number12);
+const strings = ["cwknve", "cnevnoevne", "vevrnri"];
--- getData/src/generics.ts
+++ getData/src/generics.ts
@@ -27,3 +27,14 @@ function getFirstNumber(array: number []){
 const numbers =  [1,2,3,7,90,345,6,8,9]
 const firstnumber = getFirstNumber(numbers)
 console.log(firstnumber);
+
+// generics return the type of an element whene the element is noy defined like use of any nd unknown types so in this situation generics returns a value if its a number it returns a number and the string and boolean the same espeacially in the use of arrays https://youtu.be/EcCTIExsqmI?si=WaOlJ775SXd1J0X8
+// generics used here 
+function getNumber<ElementType>(array: ElementType[]){ //single generic type
+    return array[0]
+}
+
+const number12 = [1,2,3,4,5,6,7]
+const firstNumber23 = getNumber(number12)
+
+const strings = ["cwknve","cnevnoevne", "vevrnri"]
